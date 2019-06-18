847e0370f25bead59e1f2e860f8ef166a6550567
Subodh Bhargav <S531388@nwmissouri.edu>
2019-06-18 00:01:07 -0500

    Twitter Differentiation

---
+++ Assignment_keys.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 17 22:51:11 2019
+
+@author: S531388
+"""
+
+consumer_key = 'ZWQCKrPiKLG7qXBcPMW80LlDO'
+consumer_secret = '1Wtn9o7E696mLib7P4S7jemCHaYUtp6wVLAUetqNaIgYdnTbk0'
+access_token = '1087703281168773121-2LHQhIZuvZ4mvi8LHJ6HUKB7osaIL2'
+access_secret = 'Aog0QMkoyEWi2zYuAEHCWNHoficQT4YHy2qqTjsHbjXpI'
